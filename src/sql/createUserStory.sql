9d52babd24a2826a70e22df96e4a87e1d4119121
Maison <Maison>
2016-10-31 15:18:28 +0100

    Ajout tâche 9 + MAJ kanban_Sprint1

---
+++ src/sql/createUserStory.sql
@@ -0,0 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.2deb1ubuntu0.2
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Lun 31 Octobre 2016 à 02:00
+-- Version du serveur: 5.0.75
+-- Version de PHP: 5.2.6-3ubuntu4.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `alaguitard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `UserStory`
+--
+
+CREATE TABLE IF NOT EXISTS `UserStory` (
+  `us_id` int(10) unsigned NOT NULL auto_increment,
+  `project_id` int(10) unsigned NOT NULL,
+  `description` text NOT NULL,
+  `effort` int(10) NOT NULL,
+  `piority` int(10) NOT NULL,
+  PRIMARY KEY  (`us_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `UserStory`
+--
