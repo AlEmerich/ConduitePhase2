b21b9ca49be8deda823b5c625718891c4a39f335
AlEmerich <guitard-alan@laposte.net>
2016-10-31 02:11:02 +0100

    Tâche 1 terminée. Ajout dossier doc avec kanban

---
+++ src/sql/createUserTable.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.2deb1ubuntu0.2
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Lun 31 Octobre 2016 à 02:00
+-- Version du serveur: 5.0.75
+-- Version de PHP: 5.2.6-3ubuntu4.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `alaguitard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `login` varchar(50) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `User`
+--
