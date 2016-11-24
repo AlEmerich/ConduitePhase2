8bdf62e112223160aec04fcc94e9f227f30e4f46
Matthias Duffour <matthias.duffour@gmail.com>
2016-11-24 12:20:10 +0100

    Task 1 + Update kanbanSprint3é

---
+++ src/sql/createTask.sql
@@ -0,0 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.2deb1ubuntu0.2
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Lun 31 Octobre 2016 à 02:00
+-- Version du serveur: 5.0.75
+-- Version de PHP: 5.2.6-3ubuntu4.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `alaguitard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Task`
+--
+
+CREATE TABLE IF NOT EXISTS `Task` (
+  `task_id` int(10) unsigned NOT NULL auto_increment,
+  `us_id` int(10) unsigned NOT NULL, 
+  `description` TEXT NOT NULL,
+  `cost` SMALLINT NOT NULL,
+  PRIMARY KEY  (`task_id`),
+	FOREIGN KEY(us_id) REFERENCES UserStory(us_id)
+) ENGINE=INNODB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `Task`
+--
